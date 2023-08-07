2bf855d9ac26fbdf35750b1baef0126da0766708
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-08-07 18:02:32 +0900

    read project name from env

--- export_from_scrapbox/main.ts
+++ export_from_scrapbox/main.ts
@@ -2,12 +2,12 @@
 import { exportPages, assertString } from "./deps.ts";
 
 const sid = Deno.env.get("SID");
-const exportingProjectName = "omoikane";
+const project_name = Deno.env.get("PROJECT_NAME");
 
 assertString(sid);
 
-console.log(`Exporting a json file from "/${exportingProjectName}"...`);
-const result = await exportPages(exportingProjectName, {
+console.log(`Exporting a json file from "/${project_name}"...`);
+const result = await exportPages(project_name, {
   sid,
   metadata: false,
 });
@@ -18,4 +18,8 @@ if (!result.ok) {
   throw error;
 }
 
-Deno.writeTextFile("omoikane.json", JSON.stringify(result.value, null, 2));
+Deno.writeTextFile(
+  `${project_name}.json`,
+  JSON.stringify(result.value, null, 2)
+);
+console.log(`OK, wrote "${project_name}.json"`);
