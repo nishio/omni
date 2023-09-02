49396b91e5b938f1f189613352289b847c50db84
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-09-02 10:44:57 +0900

    refactor

--- write_to_scrapbox/main.py
+++ write_to_scrapbox/main.py
@@ -1,46 +1,14 @@
-import requests
-import dotenv
-import json
-import os
+"""
+entrypoint from github actions
+"""
+
+import scrapbox_io
 import recurrent_notes
-
-dotenv.load_dotenv()
-SID = os.getenv("SID")
-
-PROJECT = os.getenv("PROJECT_NAME")
-
-
-API_ME = "https://scrapbox.io/api/users/me"
-import_api_url = f"https://scrapbox.io/api/page-data/import/{PROJECT}.json"
-
-
-def write_pages(pages):
-    cookie = "connect.sid=" + SID
-    r = requests.get(API_ME, headers={"Cookie": cookie})
-    r.raise_for_status()
-    csrfToken = r.json()["csrfToken"]
-
-    data = json.dumps({"pages": pages})
-    r = requests.post(
-        import_api_url,
-        files={"import-file": data},
-        headers={
-            "Cookie": cookie,
-            "Accept": "application/json, text/plain, */*",
-            "X-CSRF-TOKEN": csrfToken,
-        },
-    )
-    r.raise_for_status()
-
-
-def _test():
-    pages = [{"title": "Scbot Home", "lines": ["Scbot Home", "Hello world!"]}]
-    write_pages(pages)
 
 
 def main(dry=False):
     pages = recurrent_notes.main()
-    write_pages(pages)
+    scrapbox_io.write_pages(pages)
     print("write ok")
 
 
---
+++ write_to_scrapbox/scrapbox_io.py
@@ -0,0 +1,44 @@
+"""
+Scrapbox doesn't have official write API. Use import API instead.
+"""
+
+import requests
+import dotenv
+import json
+import os
+
+dotenv.load_dotenv()
+SID = os.getenv("SID")
+PROJECT = os.getenv("PROJECT_NAME")
+
+
+API_ME = "https://scrapbox.io/api/users/me"
+import_api_url = f"https://scrapbox.io/api/page-data/import/{PROJECT}.json"
+
+
+def write_pages(pages):
+    cookie = "connect.sid=" + SID
+    r = requests.get(API_ME, headers={"Cookie": cookie})
+    r.raise_for_status()
+    csrfToken = r.json()["csrfToken"]
+
+    data = json.dumps({"pages": pages})
+    r = requests.post(
+        import_api_url,
+        files={"import-file": data},
+        headers={
+            "Cookie": cookie,
+            "Accept": "application/json, text/plain, */*",
+            "X-CSRF-TOKEN": csrfToken,
+        },
+    )
+    r.raise_for_status()
+
+
+def _test():
+    pages = [{"title": "Scbot Home", "lines": ["Scbot Home", "Hello world!"]}]
+    write_pages(pages)
+
+
+if __name__ == "__main__":
+    _test()
