be35901f1e859561d40dacdf64d35913e195e831
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-09-22 14:24:26 +0900

    run local pioneer

--- local_pinoneer.sh
+++ local_pinoneer.sh
@@ -1,4 +1,6 @@
 #/bin/bash -e
+
+# make embedding from private project
 python export_from_scrapbox/main.py --project nishio-llm2023
 python make_vecs_from_json/main.py --project nishio-llm2023 --is-private
 
@@ -8,7 +10,9 @@ python make_vecs_from_json/main.py --project nishio-books-tmp --is-private
 python export_from_scrapbox/main.py --project omni-private
 python make_vecs_from_json/main.py --project omni-private --is-private
 
+# download other public embedded project
 python download_latest_release/main.py --repo omoikane-embed-unnamed-project
 python download_latest_release/main.py --repo omoikane-embed
+
 
 python write_to_scrapbox/iterative_commenter.py --pickles all --pioneer-loop-private
